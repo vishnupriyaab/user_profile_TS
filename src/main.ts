0466470837ca918cb8853d4f6676b44d03732e5d
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2024-08-10 16:18:38 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import TemplateList from './templates/TemplateList.ts';
+
+const initAPP = async(): Promise<void> => {
+  const UserProfile = TemplateList.instance;
+
+  const generateBtn = document.getElementById('Generate') as HTMLButtonElement;
+
+  generateBtn.addEventListener('click', async (): Promise<void> => {
+    try {
+      generateBtn.textContent = 'Processing...'
+      await UserProfile.getData();
+      generateBtn.textContent = 'Generate Another'
+    } catch (err: any) {
+      console.error(err.message);
+      setTimeout(() => {
+        location.reload();
+      }, 2000);
+    }
+  });
+
+  await UserProfile.getData();
+}
+
+document.addEventListener('DOMContentLoaded', initAPP);
